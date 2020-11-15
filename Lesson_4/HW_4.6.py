545c3575720576e6fb9e6cca4c0801a1ad8b03ea
Mikhail <2068811@gmail.com>
2020-11-15 23:52:17 +0500

    The fourth homework исправленная задача 4.6.1

--- Lesson_4/HW_4.6.py
+++ Lesson_4/HW_4.6.py
@@ -5,16 +5,20 @@ from itertools import count, cycle
 from time import sleep
 first = int(input("Введите начальное число: "))
 last = int(input("Введите конечное число: "))
-for x in range(first, last+1):
-    print(x)
-    sleep(1)
-
-n = int(input("Введите количество циклов: "))
-count = 1
-a = ["Рассказать тебе", "сказку про", "белого бычка?"]
-for x in cycle(a):
-    if count > (n * len(a)):
+i = 1
+for x in count(first):
+    if i > (last-first+1):
         break
     print(x)
     sleep(1)
-    count += 1
+    i += 1
+
+n = int(input("Введите количество циклов: "))
+i = 1
+a = ["Рассказать тебе", "сказку про", "белого бычка?"]
+for x in cycle(a):
+    if i > (n * len(a)):
+        break
+    print(x)
+    sleep(1)
+    i += 1
